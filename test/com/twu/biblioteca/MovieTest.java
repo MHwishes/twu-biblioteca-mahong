1eb069907771c975154457feed02f1ccabfe7b0e
hma <hma@thoughtworks.com>
2017-07-30 10:26:41 +0800

    fix movie function name

--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class MovieTest {
 
     @Test
-    public void should_get_detail_for_book() {
+    public void should_get_detail_for_movie() {
         Movie movie = new Movie("movie","2015","zhangdab","2");
 
         assertThat(movie.getMovieDetail(),containsString("movie"));
