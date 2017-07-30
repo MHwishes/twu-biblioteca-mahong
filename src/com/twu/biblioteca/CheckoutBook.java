8b158e82149dea049e5aba09b8999bbc6b4916f8
hma <hma@thoughtworks.com>
2017-07-30 08:41:14 +0800

    add checkoutBook & Return Book

---
+++ src/com/twu/biblioteca/CheckoutBook.java
@@ -0,0 +1,28 @@
+package com.twu.biblioteca;
+
+import java.util.Scanner;
+
+public class CheckoutBook implements MenuOption{
+    private Library library;
+    private Console console;
+
+     CheckoutBook(Library library,Console console) {
+        this.library = library;
+        this.console=console;
+    }
+
+    @Override
+    public void execute() {
+       library.displayBooksWithNumbers();
+        System.out.print("Choose a book: ");
+
+        String n=console.getUserInput();
+
+        if(library.checkoutBook(Integer.parseInt(n))) {
+            System.out.println("Thank you! Enjoy your book.");
+        } else {
+            System.out.println("That book is not available.");
+            System.out.print("Please re-enter a valid book choice: ");
+        }
+    }
+}
---
+++ src/com/twu/biblioteca/ReturnBook.java
@@ -0,0 +1,28 @@
+package com.twu.biblioteca;
+
+import java.util.Scanner;
+
+public class ReturnBook implements MenuOption{
+    private Library library;
+    private Console console;
+
+    ReturnBook(Library library,Console console) {
+        this.library = library;
+        this.console=console;
+    }
+
+    @Override
+    public void execute() {
+        library.displayCheckedOutBooksWithNumbers();
+        System.out.print("Choose a book: ");
+
+        String n=console.getUserInput();
+
+        if(library.returnBook(Integer.parseInt(n))) {
+            System.out.println("Thank you for returning the book.");
+        } else {
+            System.out.println("That is not a valid book to return.");
+            System.out.print("Please re-enter a valid book choice: ");
+        }
+    }
+}
